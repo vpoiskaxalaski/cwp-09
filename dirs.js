3678a981ef499cc81b38a697456d914c30fa2224
Alena <awff@mail.ru>
2018-10-18 22:00:05 +0300

    dirs.js

---
+++ dirs.js
@@ -0,0 +1,32 @@
+const Promise = require('bluebird');
+const path = require('path');
+const fs = Promise.promisifyAll(require('fs'));
+
+const dirs = [
+	'dir-1/dir-1-1',
+	'dir-1/dir-1-2',
+	'dir-1/dir-1-2/dir-1-2-1',
+	'dir-2/dir-2-1/dir-2-1-1',
+	'dir-2/dir-2-2/dir-2-2-1',
+	'dir-2/dir-2-1/dir-2-2-2/dir-2-2-2-1',
+	'dir-3/dir-3-1',
+	'dir-3',
+	'dir-3/dir-3-2/dir-3-2-1',
+	'dir-3/dir-3-3/dir-3-3-1'
+];
+
+let arrOfPaths = [];
+
+dirs.forEach((dir) => {
+	let p = './gen/';
+	dir.split('/')
+		.forEach((dir) => {
+			p += dir + path.sep;
+			if (arrOfPaths.indexOf(p) < 0)
+				arrOfPaths.push(p);
+		})
+});
+
+Promise.mapSeries(arrOfPaths, (path) => {
+	return fs.mkdirAsync(path);
+});
